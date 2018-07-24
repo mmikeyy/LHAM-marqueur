89b4259ca7aa68549737f92504a32872df352c2e
Mmikeyy <git_mail@loukilou.com>
2018-07-23 21:00:56 -0400

    ajout de error tracking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -70,6 +70,34 @@ import * as moment from 'moment';
 import { ForfaitComponent } from '../pages/forfait/forfait.component';
 import { CollecterStatsComponent } from '../pages/collecterStats/collecterStats.component';
 
+import { Pro } from '@ionic/pro';
+import { Injectable, Injector } from '@angular/core';
+
+Pro.init('3F2F73C3', {
+  AppVersion: '0.0.1'
+});
+@Injectable()
+export class MyErrorHandler implements ErrorHandler {
+  ionicErrorHandler: IonicErrorHandler;
+
+  constructor(injector: Injector) {
+    try {
+      this.ionicErrorHandler = injector.get(IonicErrorHandler);
+    } catch(e) {
+      // Unable to get the IonicErrorHandler provider, ensure
+      // IonicErrorHandler has been added to the providers list below
+    }
+  }
+
+  handleError(err: any): void {
+    Pro.monitoring.handleNewError(err);
+    // Remove this if you want to disable Ionic's auto exception handling
+    // in development mode.
+    this.ionicErrorHandler && this.ionicErrorHandler.handleError(err);
+  }
+}
+
+
 @NgModule({
   declarations: [
     FilterPipe,
@@ -182,7 +210,9 @@ import { CollecterStatsComponent } from '../pages/collecterStats/collecterStats.
     StatusBar,
     TranslateService,
     XhrService,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    // {provide: ErrorHandler, useClass: IonicErrorHandler},
+    IonicErrorHandler,
+    [{ provide: ErrorHandler, useClass: MyErrorHandler }],
     MatchsService,
     JoueursService,
     FmService,
